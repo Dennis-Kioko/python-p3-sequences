17acdcf0b72dc4b6e0da3be90ece40e27c6c1161
denniskioko <kiokodennis6990@gmail.com>
2024-02-28 14:41:25 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    # initialize fibonacci_sequence with first two no.s
+    fibonacci_sequence = [0,1]
+    
+    for n in range(length -2):
+        fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+    print(fibonacci_sequence[:length])
+
+print_fibonacci(9)
